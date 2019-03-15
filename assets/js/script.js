bcec60d7c79a08318ffd58376878f1917eb013e6
AnteVuletic <antevuletic1995@gmail.com>
2019-03-15 09:22:57 +0100

    Favorites now show correct number of favorited elements and are saved while filtering until browser is closed or refreshed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,91 @@
 let dropdown = document.querySelectorAll(".dropdown__list");
 let contentWrapper = document.querySelector(".main__offers");
-let favouritesCounter = 0;
 let counterHTML = document.createElement("span");
 let filteredCount = document.querySelector("#bonus");
 let filterList = [];
 
 counterHTML.classList.add("favourites");
-
 let content = [
     {
+        id: 1,
         img: "./assets/images/offer.jpg",
         description:"TALL",
         paragraph:"Wrangler small logo crew neck t-shirt in white",
         price:"£16.63",
-        color: "white"
+        color: "white",
+        favorited: false
     },
     {
+        id: 2,
         img: "./assets/images/offer.jpg",
         description: "TALL",
         paragraph:"Wrangler logo chest stripe rugby polo in blue/white",
         price:"£50.63",
-        color: "blue"
+        color: "blue",
+        favorited: false
     },
     {
+        id: 3,
         img: "./assets/images/offer.jpg",
         description: "TALL",
         paragraph: "Wrangler kobel retro large logo ringer t-shirt in white",
         price:"£20.97",
-        color: "white"
+        color: "white",
+        favorited: false
     },
     {
+        id: 4,
         img: "./assets/images/offer.jpg",
         description: "TALL",
         paragraph: "Lyle & Scott polo burgundy",
         price: "£44.12",
-        color: "green"
+        color: "green",
+        favorited: false
     },
     {
+        id: 5,
         img: "./assets/images/offer.jpg",
         description: "TALL",
         paragraph: "COLLUSION Unisex Long sleeve t-shirt with graphic print in neon green",
         price: "£12.29",
-        color: "green"
+        color: "green",
+        favorited: false
     },
     {
+        id: 6,
         img: "./assets/images/offer.jpg",
         description: "TALL",
         paragraph: "ASOS DESIGN Disney oversized t-shirt with rainbow",
         price: "£22.42",
-        color: "blue"
+        color: "blue",
+        favorited: false
     },
     {
+        id: 7,
         img: "./assets/images/offer.jpg",
         description: "TALL",
         paragraph: "COLLUSION Unisex oversized t-shirt with back print",
         price: "£10.12",
-        color: "blue"
+        color: "blue",
+        favorited: false
     },
     {
+        id: 8,
         img: "./assets/images/offer.jpg",
         description: "TALL",
         paragraph: "ASOS DESIGN Mickey reloxed t-shirt with retro print",
         price: "£18.08",
-        color: "green"
+        color: "green",
+        favorited: false
     }
 ];
+let renderedItems = content;
 BuildFilterItem(dropdown,[{type:"Grey",count:1014},{type:"Navy",count: 1014},{type:"Blue",count:1014},{type:"Green",count:1014}]);
-AddContent(contentWrapper,content);
+AddContent(contentWrapper);
 contentWrapper.addEventListener('mouseover',(event)=>{
     let prevItem = contentWrapper.querySelector(".item__img-description--show");
     if(prevItem)
-        prevItem.classList.remove("item__img-description--show");
+    prevItem.classList.remove("item__img-description--show");
     if(event.target.classList == "item__img"){
         event.composedPath()[1].querySelector(".item__img-description").classList.add("item__img-description--show");
     }
@@ -79,11 +94,11 @@ contentWrapper.addEventListener('mouseover',(event)=>{
 contentWrapper.addEventListener('click',(event)=>{
     if(event.target.classList.value.includes("item__heart-full")){
         if(event.target.classList.value.includes("item__heart-full--show")){
-            favouritesCounter--;
             event.target.classList.remove("item__heart-full--show");
+            content[content.findIndex((object) => {return object.id === Number(event.target.id)})].favorited = false;
         }else{
             event.target.classList.add("item__heart-full--show");
-            favouritesCounter++;
+            content[content.findIndex((object) => {return object.id === Number(event.target.id)})].favorited = true;
         }
         RenderCount();
         return;
@@ -128,7 +143,6 @@ contentWrapper.addEventListener('click',(event)=>{
     }
 });
 dropdown[1].addEventListener("click",(event)=>{
-    console.log(event.target.classList.value)
     if(event.target.classList.value.includes("dropdown__list__item")){
         if(event.target.classList.value.includes("dropdown__list__item--clicked")){
             event.target.classList.remove("dropdown__list__item--clicked");
@@ -140,22 +154,23 @@ dropdown[1].addEventListener("click",(event)=>{
             filterList.push(event.target.innerText.toLowerCase());
         }
     }
-    favouritesCounter = 0;
     filteredCount.innerText = filterList.length + " selected";
     if(filterList.length === 0){
-        AddContent(contentWrapper,content);
+        renderedItems = content;
+        AddContent(contentWrapper);
     }else{
-        AddContent(contentWrapper,content.filter((item)=>{
+        renderedItems = content.filter((item)=>{
             for(let filter of filterList){
                 if(filter.includes(item.color))
                     return true
             }
             return false;
-        }));
+        });
+        AddContent(contentWrapper);
     }
 });
 function RenderCount(){
-    counterHTML.innerText = "Favourties: " + favouritesCounter;
+    counterHTML.innerText = "Favourties: " + renderedItems.filter((item)=>{return item.favorited}).length;
     contentWrapper.insertBefore(counterHTML,contentWrapper.childNodes[0]);
 }
 function BuildFilterItem(wrapper,items){ 
@@ -176,23 +191,37 @@ function BuildFilterItem(wrapper,items){
     });
 }
 
-function AddContent(contentWrapper,itemInfo){
+function AddContent(contentWrapper){
     contentWrapper.innerHTML = "";
-    itemInfo.map((item,index)=>{
-        itemInfo[index].html =   
-        `<div class="offers__item">
-        <img class="item__img" src=${item.img} alt="Offer" />
-        <img class="item__heart-shape" src="./assets/images/heart-shape.png" alt="Hearth Shape">
-        <img class="item__heart-full" src="./assets/images/heart-full.png" alt="Hearth Full">
-        <p class="item__img-description">${item.description}</p>
-        <p class="item__paragraph">
-        ${item.paragraph}
-        </p>
-        <span class="item__price">${item.price}</span>
-        </div>`
+    renderedItems.map((item,index)=>{
+        if(item.favorited){
+            renderedItems[index].html =   
+            `<div class="offers__item">
+            <img class="item__img" src=${item.img} alt="Offer" />
+            <img class="item__heart-shape" src="./assets/images/heart-shape.png" alt="Hearth Shape">
+            <img id=${item.id} class="item__heart-full item__heart-full--show" src="./assets/images/heart-full.png" alt="Hearth Full">
+            <p class="item__img-description">${item.description}</p>
+            <p class="item__paragraph">
+            ${item.paragraph}
+            </p>
+            <span class="item__price">${item.price}</span>
+            </div>`;
+        }else{
+            renderedItems[index].html =   
+            `<div class="offers__item">
+            <img class="item__img" src=${item.img} alt="Offer" />
+            <img class="item__heart-shape" src="./assets/images/heart-shape.png" alt="Hearth Shape">
+            <img id=${item.id} class="item__heart-full" src="./assets/images/heart-full.png" alt="Hearth Full">
+            <p class="item__img-description">${item.description}</p>
+            <p class="item__paragraph">
+            ${item.paragraph}
+            </p>
+            <span class="item__price">${item.price}</span>
+            </div>`;
+        }
     });
-    itemInfo.forEach((item)=>{
+    renderedItems.forEach((item)=>{
         contentWrapper.innerHTML += item.html;
     });
-    RenderCount();
+    RenderCount(renderedItems);
 }
